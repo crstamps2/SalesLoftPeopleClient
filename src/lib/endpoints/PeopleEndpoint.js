3dcdef8fa121aba4fd43fa15039db8439a0cd4aa
Cody Stamps <crstamps2@gmail.com>
2018-05-07 12:30:02 -0400

    add a people endpoint w/ tests

---
+++ src/lib/endpoints/PeopleEndpoint.js
@@ -0,0 +1,11 @@
+export default class PeopleEndpoint {
+  constructor(axiosClient) {
+    this.http = axiosClient
+  }
+
+  /**
+   * Gets a list of SalesLoft People 
+   * @return {Object}        
+   */
+  getAll = () => this.http.get('/people')
+}
---
+++ src/lib/endpoints/__tests__/PeopleEndpoint.test.js
@@ -0,0 +1,27 @@
+import PeopleEndpoint from '../PeopleEndpoint'
+
+let mockResponse
+let mockAxios
+let endpoint
+
+beforeEach(() => {
+  mockResponse = {
+    data: [
+        {person: 1}
+    ],
+  }
+  mockAxios = {
+    get: jest.fn(() => Promise.resolve(mockResponse)),
+    post: jest.fn(() => Promise.resolve(mockResponse)),
+  }
+  endpoint = new PeopleEndpoint(mockAxios)
+})
+
+describe('#get', () => {
+  it('performs a GET request to the /people endpoint', () => {
+    return endpoint.getAll().then(response => {
+      expect(mockAxios.get.mock.calls.length).toEqual(1)
+      expect(mockAxios.get.mock.calls[0][0]).toEqual(`/people`)
+    })
+  })
+})
