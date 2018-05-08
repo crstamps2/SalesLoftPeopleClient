83f7d63a1654c6ebb170332aea4a6a961e41f1b6
Cody Stamps <crstamps2@gmail.com>
2018-05-07 20:05:04 -0400

    hook up router to app
    add tests to ensure the app home page still renders

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import {BrowserRouter as Router, Route, Switch} from 'react-router-dom'
 import logo from './logo.svg';
 import './App.css';
 
@@ -6,6 +7,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
+        <Router>
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
@@ -13,6 +15,7 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+        </Router>
       </div>
     );
   }
--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,45 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import React from 'react'
+import {BrowserRouter, Route, Router} from 'react-router-dom'
+import {shallow, mount} from 'enzyme'
+import {difference} from 'lodash'
+import App from './App'
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
-});
+let component
+
+const renderComponent = (fullMount = false) => {
+  const renderer = fullMount ? mount : shallow
+  return component = renderer(<App />)
+}
+
+beforeEach(() => {
+  renderComponent()
+})
+
+it('contains a router', () => {
+  expect(component.find(BrowserRouter).length).toEqual(1)
+})
+
+describe('routes', () => {
+  it('includes the expected routes', () => {
+    renderComponent(false)
+    const expectedRoutes = [
+      // '/people',
+    ]
+
+    const routes = routesByPath()
+    const paths = Object.keys(routes)
+
+    expect(difference(expectedRoutes, paths).length).toEqual(0)
+  })
+})
+
+const routesByPath = () => {
+  const result = {}
+  const routes = [
+    ...component.find(Route),
+  ]
+  routes.forEach(route => {
+    result[route.props['path']] = route
+  })
+  return result
+}
